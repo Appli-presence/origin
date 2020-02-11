bc3366bcdc7c0a1e7fe614802e1ada60fd7bd909
NANOTECH <redalamtaoui@gmail.com>
2020-02-11 14:33:27 +0100

    Update PromotionController.php

--- app/Http/Controllers/PromotionController.php
+++ app/Http/Controllers/PromotionController.php
@@ -5,8 +5,25 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\promotion;
+use App\categorie;
+use Auth;
+use Session;
+
 class PromotionController extends Controller
 {
     //create
-    
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    public function index()
+    {
+        $categorie=categorie::all();
+        $produit=produit::paginate(6);
+        return view('index',[
+            "categories"=>$categorie,
+            "produits"=>$produit
+            ]);
+    }
 }

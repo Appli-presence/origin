a4d64a2d03430b5c59b6d98c88b452097a3d418c
juliengenel <48075684+juliengenel@users.noreply.github.com>
2020-02-11 14:31:04 +0100

    Ajout delete

--- app/Http/Controllers/PromotionController.php
+++ app/Http/Controllers/PromotionController.php
@@ -3,9 +3,17 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Promotion;
 use Illuminate\Http\Request;
 
-class PromotionController extends Controller
-{
-    //
+class PromotionController extends Controller {
+
+    public function delete($id)
+    {
+        $delete = Promotion::where('id', $id)
+            ->get();
+        foreach ($delete as $d) {
+            $d->delete();
+        }
+    }
 }

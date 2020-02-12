30752d1b05699d83f211abf4804375760db94a3f
Balair Gauthier <44637482+gauthierbal@users.noreply.github.com>
2020-02-12 10:51:03 +0100

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,15 +33,17 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-
-                    @if ( Auth::user()->role == 'ADMIN')
-                        <a href="/studentList"><li>Gestion étudiants</li></a>
-                        <a href="/promotion"><li>Promotions</li></a>
-                        <a href="/cours"><li>Cours</li></a>
-                        <a href="/teacher"><li>Prof</li></a>
+                    @guest
                     @else
-                        <a href="./teacherCall"><li>Gestion appel</li></a>
-                    @endIf
+                        @if ( Auth::user()->role == 'ADMIN')
+                            <a href="./studentList"><li>Gestion étudiants</li></a>
+                            <a href="./promotion"><li>Promotions</li></a>
+                            <a href="./cours"><li>Cours</li></a>
+                            <a href="./teacher"><li>Prof</li></a>
+                        @else
+                            <a href="./teacherCall"><li>Gestion appel</li></a>
+                        @endIf
+                    @endguest
                     
                     </ul>
                     <!-- Right Side Of Navbar -->

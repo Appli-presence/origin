6708ef91af36ee329dd4f504008c09c9f69e5cb9
juliengenel <48075684+juliengenel@users.noreply.github.com>
2020-02-12 08:46:59 +0100

    mise a jour du style

--- resources/views/promotion/update-promo.blade.php
+++ resources/views/promotion/update-promo.blade.php
@@ -1,6 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+
 <form action="{{ route('edit', ['id' => $promotion->promotionId]) }}" method="POST">
     @csrf
     <input type="text" name="libelle" value="{{ $promotion->libelle }}">
     <input type="submit" value="Envoyer">
 </form>
 
+@endsection

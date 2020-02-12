9233932349d3771786b3023e0b700b4b4739f1e4
Balair Gauthier <44637482+gauthierbal@users.noreply.github.com>
2020-02-12 16:26:51 +0100

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,7 @@
 <div class="container">
     <h1>Fellapel</h1>
     <p>Challenge web LP DIM février 2020</p>
+    <a target="_blank" href="https://docs.google.com/presentation/d/1FZiPcRFCeBDDa-nmDs-Np9iAmnduaA4of25qfyJtMxU/edit?usp=sharing"><p>Accéder à la présentation du site</p></a>
     <div>
         <img src="https://media.discordapp.net/attachments/623428381257629696/677163022460125253/20200212_100329.jpg?width=901&height=676" alt="" style="box-shadow: 10px 5px 5px rgba(0, 0, 255, .2);width: 60%;">
     </div>
